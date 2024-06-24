45e6be2f2ed0e164b14f7600ede0297f68c7db04
Wangui <elswangui@gmail.com>
2024-06-24 18:26:50 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+
+const num3 = 9
+const num4 = 5
+const mod = num3 % num4;
+
+
+
+const numbers = [3, 16, 20, 10, 1];
+const max = Math.max(3, 16, 20, 10, 1);
+console.log(max);
+
